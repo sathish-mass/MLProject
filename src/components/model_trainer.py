0e7f87bbe3b2c28a274b2e53ff3adb8669db1149
sathesh_muppudathi <sathishmuppudathi@gmail.com>
2023-10-25 17:51:05 +0530

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -18,21 +18,18 @@ from src.logger import logging
 
 from src.utils import save_object,evaluate_models
 
-
-
 @dataclass
-class ModelTrainerConfig():
-    trained_model_file_path=os.path.join('artifacts','model.pkl')
-    
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pkl")
 
-class ModelTrainer():
+class ModelTrainer:
     def __init__(self):
-        self.model_trainer_config= ModelTrainerConfig()
-        
+        self.model_trainer_config=ModelTrainerConfig()
+
+
     def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Splitting training and test data")
-            
+            logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
                 train_array[:,:-1],
                 train_array[:,-1],
@@ -46,8 +43,7 @@ class ModelTrainer():
                 "Linear Regression": LinearRegression(),
                 "XGBRegressor": XGBRegressor(),
                 "AdaBoost Regressor": AdaBoostRegressor(),
-            }      
-            
+            }
             
             params={
                 "Decision Tree": {
@@ -81,30 +77,37 @@ class ModelTrainer():
                 }
                 
             }
-                  
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,params=params)
             
-            model_report: dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test, models=models, params=params)
-            
-            best_model_score=max(sorted(model_report.values()))
-            
-            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-            
-            best_model=models[best_model_name]
-            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
+
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
+
             if best_model_score<0.6:
-                raise CustomException('No best Model Found')
-            
+                raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
-            
+
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
                 obj=best_model
             )
-            
+
             predicted=best_model.predict(X_test)
-            
-            r2_square=r2_score(y_test,predicted)
+
+            r2_square = r2_score(y_test, predicted)
             return r2_square
             
+
+
+
+            
         except Exception as e:
-            raise CustomException(e, sys.exc_info())
+            raise CustomException(e,sys.exc_info())
